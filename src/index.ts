566ae0d19aed7426ede2d65f75a19aace8e206a5
OUALI NASREDDINE <55834100+nasro-dadi@users.noreply.github.com>
2024-11-30 23:31:36 +0100

    feat: Enhance search endpoint with query validation and CORS support

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import { searchController } from "./controllers/search-controller";
+import { validateQuery } from "./middlewares/validator";
+import { searchQuerySchema } from "./schemas/search-schema";
+
+dotenv.config();
 
 const app = express();
-const port = 3001;
+const corsWhitelist = process.env.CORS_WHITELIST;
+app.use(
+  cors({
+    origin: corsWhitelist?.split(","),
+    methods: ["GET"],
+    allowedHeaders: "*",
+  })
+);
+const port = process.env.PORT ?? 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.get("/search", searchController);
+app.get("/search", validateQuery(searchQuerySchema), searchController);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
