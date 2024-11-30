0804ff709f5451c12503d9c09d4856b753246cd7
OUALI NASREDDINE <55834100+nasro-dadi@users.noreply.github.com>
2024-11-30 23:32:06 +0100

    feat: Validate search query parameters and adjust pagination logic

--- src/controllers/search-controller.ts
+++ src/controllers/search-controller.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
+import { searchQuerySchema } from "../schemas/search-schema";
 import { searchBooks } from "../services/book-service";
 
 export const searchController = async (req: Request, res: Response) => {
-  const { q, page = 1, limit = 10 } = req.query;
-  const offset = (Number(page) - 1) * Number(limit);
+  const validatedQuery = searchQuerySchema.parse(req.query);
+  const { q, page = "1", limit = "5", sort } = validatedQuery;
+  const offset = Number(page) * Number(limit);
 
   try {
-    const booksData = await searchBooks(q as string, Number(limit), offset);
+    const booksData = await searchBooks({
+      q: q,
+      limit: limit,
+      page: offset.toString(),
+      sort: sort,
+    });
     res.json({
       total: booksData.numFound,
       page: Number(page),
